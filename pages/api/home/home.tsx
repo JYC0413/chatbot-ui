e7c9f299fba288e4a695bc5f96cf29e252a10fb4
JYC0413 <juyichen0413@foxmail.com>
2023-10-03 20:50:07 +0800

    fix

--- pages/api/home/home.tsx
+++ pages/api/home/home.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import { useQuery } from 'react-query';
 
-import { GetServerSideProps } from 'next';
+// import { GetServerSideProps } from 'next';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Head from 'next/head';
@@ -400,37 +400,37 @@ const Home = ({
 };
 export default Home;
 
-export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
-  const defaultModelId =
-    (process.env.DEFAULT_MODEL &&
-      Object.values(OpenAIModelID).includes(
-        process.env.DEFAULT_MODEL as OpenAIModelID,
-      ) &&
-      process.env.DEFAULT_MODEL) ||
-    fallbackModelID;
-
-  let serverSidePluginKeysSet = false;
-
-  const googleApiKey = process.env.GOOGLE_API_KEY;
-  const googleCSEId = process.env.GOOGLE_CSE_ID;
-
-  if (googleApiKey && googleCSEId) {
-    serverSidePluginKeysSet = true;
-  }
-
-  return {
-    props: {
-      serverSideApiKeyIsSet: !!process.env.OPENAI_API_KEY,
-      defaultModelId,
-      serverSidePluginKeysSet,
-      ...(await serverSideTranslations(locale ?? 'en', [
-        'common',
-        'chat',
-        'sidebar',
-        'markdown',
-        'promptbar',
-        'settings',
-      ])),
-    },
-  };
-};
+// export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+//   const defaultModelId =
+//     (process.env.DEFAULT_MODEL &&
+//       Object.values(OpenAIModelID).includes(
+//         process.env.DEFAULT_MODEL as OpenAIModelID,
+//       ) &&
+//       process.env.DEFAULT_MODEL) ||
+//     fallbackModelID;
+//
+//   let serverSidePluginKeysSet = false;
+//
+//   const googleApiKey = process.env.GOOGLE_API_KEY;
+//   const googleCSEId = process.env.GOOGLE_CSE_ID;
+//
+//   if (googleApiKey && googleCSEId) {
+//     serverSidePluginKeysSet = true;
+//   }
+//
+//   return {
+//     props: {
+//       serverSideApiKeyIsSet: !!process.env.OPENAI_API_KEY,
+//       defaultModelId,
+//       serverSidePluginKeysSet,
+//       ...(await serverSideTranslations(locale ?? 'en', [
+//         'common',
+//         'chat',
+//         'sidebar',
+//         'markdown',
+//         'promptbar',
+//         'settings',
+//       ])),
+//     },
+//   };
+// };
